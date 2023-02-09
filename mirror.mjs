4d4a9c4b3070b23baaa54e2f56637a6401ef2899
Graham Hotchkiss <graham.hotchkiss@nature.scot>
2023-02-09 14:15:53 +0000

    Save the big lump of JSON as a file
    - NB the code updates the JSON big file

--- mirror.mjs
+++ mirror.mjs
@@ -1,3 +1,10 @@
+//#region Imports
+
+import { mkdir, writeFile } from 'node:fs/promises';
+import { resolve } from 'node:path';
+
+//#endregion
+
 //#region Constants
 
 /**
@@ -5,23 +12,55 @@
  */
  const realApiEndpoint = 'https://apps.snh.gov.uk/SCMFeed/sites';
 
+/**
+ * Where to save the mirrored files to.
+ */
+ const baseRoot = 'mirror';
+
+ /**
+  * Where to re-host the mirrored files from.
+  */
+ const baseUrl = 'scm-feed/sites';
+
 //#endregion
  
-//#region method to fetch data from real API
+//#region Method to fetch data from real API
 
 const getDataFromRealAPI = async () => {
     const headers = new Headers();
     headers.append('Accept', 'application/json');
-    const response = await fetch(realApiEndpoint, headers);
-    return response;
+    const response = await fetch(realApiEndpoint, {headers});
+    // TODO: We are going to add sanity checks here
+    const apiJson = await response.json();
+    return apiJson;
+}
+ 
+//#endregion 
+
+//#region Formatters
+
+const prettyJson = (jsonData) => {
+    return JSON.stringify(jsonData, undefined, '\t');
 }
  
 //#endregion
 
 //#region Main Script
 
-const realApiData = await getDataFromRealAPI();
-console.log('here2', realApiData.ok);
- 
+// Create the base directory for saving and serving the mirrored data.
+const basePath = resolve(baseRoot, baseUrl);
+await mkdir(basePath, { recursive: true });
+
+// Get the big lump of data
+console.log('Fetching the data from the real API');
+const realApiJsonData = await getDataFromRealAPI();
+
+// Save it as an 'index' file.
+console.log('Saving the main file');
+const filePath = resolve(baseRoot, baseUrl, 'index.json');
+await writeFile(filePath, prettyJson(realApiJsonData), { encoding: 'utf8' });
+
+console.log('All done!');
+
 //#endregion
  
